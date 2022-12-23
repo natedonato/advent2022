d7255f7bbf9cfe0e6b452ea070be9795da08356e
Nate Donato <natedonato@gmail.com>
2022-12-22 23:19:44 -0800

    22 (1/2) and 23

---
+++ 22/1.js
@@ -0,0 +1,182 @@
+const fs = require("fs");
+let input = fs.readFileSync("22/input.txt", "utf-8").split("\n");
+let directions = input.pop();
+input.pop();
+input = input.map(el => el.split(''));
+
+let y = 0;
+let x = 0;
+while(input[y][x] === " "){
+ x += 1;
+}
+
+
+// x,y
+let vectors = [
+  [1, 0],  // right
+  [0, 1],  // down
+  [-1, 0], // left 
+  [0, -1], // up
+];
+
+let wraps = [wrapRight, wrapDown, wrapLeft, wrapUp];
+
+
+let dir = 0;
+let vector = vectors[dir];
+
+function getNextSteps(){
+  let steps = ""
+  while(directions.length > 0 && !isNaN(+directions.charAt(0))){
+    steps += directions.charAt(0);
+    directions = directions.substring(1)
+  }
+  return +steps;
+}
+
+function getNextTurn(){
+  if(directions.length === 0){
+    return "END";
+  }
+  if(directions.charAt(0) === "L"){
+    dir -= 1;
+    if(dir < 0){
+      dir = 3;
+    }
+  }else{
+    dir += 1;
+    if(dir > 3){
+      dir = 0;
+    }
+  }
+  vector = vectors[dir]
+
+  directions = directions.substring(1);
+}
+
+
+
+/////
+// parse next direction
+// turn with appropriate vector
+// move in direction
+  // if hit a rock. stop
+  // if hit an edge and next edge is not a rock, wrap around
+let moving = true;
+while(moving){
+  let steps = getNextSteps();
+
+
+  move(steps);
+  // printState();
+  // console.log(x,y)
+
+  if(getNextTurn() === "END"){
+    moving = false;
+
+  }
+}
+
+
+
+function move(steps) {
+
+  // console.log("moving", steps)
+  // console.log("starting", x,y)
+  // console.log('vector', vector)
+  for(let i = 0; i < steps; i++){
+    let [nextX, nextY] = [x + vector[0],y + vector[1]];
+    [nextX, nextY] = wraps[dir](nextX, nextY)
+    if(input[nextY][nextX] === "#"){
+      return;
+    }else{
+      x = nextX;
+      y = nextY;
+    }
+  }
+
+  return;
+}
+
+
+function wrapRight(x,y){
+  // console.log('wrapping', x,y, "right")
+  while (input[y][x] !== "." && input[y][x] !== "#") {
+    if (input[y][x] === undefined) {
+      x = 0;
+    } else if (input[y][x] === " ") {
+      x += 1;
+    }
+  }
+
+  // console.log("wrapped", x,y)
+  return([x,y]);
+
+}
+
+function wrapLeft(x,y){  
+  // console.log("wrapping", x, y, "left");
+  while(input[y][x] !== "." && input[y][x] !== "#"){
+    if(input[y][x] === undefined){
+      x = input[y].length - 1;
+    }else if(input[y][x] === " "){
+      x -= 1;
+    }
+  }
+  return([x,y])
+};
+
+function wrapDown(x,y){
+  //  console.log("wrapping", x, y, "down");
+  while(!input[y] || input[y][x] !== "." && input[y][x] !== "#"){
+    // console.log("y=", input[y])
+    if(input[y] === undefined){
+      y = 0;
+    }else if(input[y][x] === " " || input[y][x] === undefined){
+      y += 1;
+    }
+  }
+  // console.log('wrapped',x,y)
+  return([x,y])
+};
+
+function wrapUp(x,y){
+  //  console.log("wrapping", x, y, "up");
+  while(!input[y] || input[y][x] !== "." && input[y][x] !== "#"){
+    if(input[y] === undefined){
+      y = input.length - 1;
+    }else if(input[y][x] === " " || input[y][x] === undefined){
+      y -= 1;
+    }
+  }
+  return([x,y])
+};
+
+
+
+function printState(){
+  for(let i = 0; i < input.length; i++){
+    let str = "";
+    for(let j = 0; j < input[i].length; j++){
+      if(j === x && i === y){
+        str += "X"
+      }else{
+        str += input[i][j]
+      }
+    }
+    console.log(str)
+  }
+}
+
+let dirScores = [
+  0,
+  1,
+  2,
+  3
+]
+
+console.log("x / col", x)
+console.log("y / row", y)
+console.log("dir", dir)
+
+console.log("finalScore, ", 1000 * (y+1) + 4 * (x+1) + dirScores[dir] )
---
+++ 23/2.js
@@ -0,0 +1,197 @@
+const fs = require("fs");
+let input = fs
+  .readFileSync("23/input.txt", "utf-8")
+  .split("\n")
+  .map((el) => el.split(""));
+
+let elves = {};
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < input[0].length; j++) {
+    if (input[i][j] === "#") {
+      elves[`${i},${j}`] = "standing";
+    }
+  }
+}
+
+let rounds = 0;
+
+function anyNeighbors(i, j) {
+  // console.log(i,j)
+
+  // around
+  let neighbors = [
+    // above
+    [i - 1, j - 1],
+    [i - 1, j],
+    [i - 1, j + 1],
+
+    // sides
+    [i, j - 1],
+    [i, j + 1],
+
+    // below
+    [i + 1, j - 1],
+    [i + 1, j],
+    [i + 1, j + 1],
+  ];
+
+  for (const coord of neighbors) {
+    let [col, row] = coord;
+    if (elves[`${col},${row}`] !== undefined) {
+      // console.log('has neighbors');
+      return true;
+    }
+  }
+  // console.log('no neighbors');
+  return false;
+}
+
+function proposeMove(i, j) {
+  let north = [
+    [i - 1, j - 1],
+    [i - 1, j],
+    [i - 1, j + 1],
+  ];
+  let south = [
+    [i + 1, j - 1],
+    [i + 1, j],
+    [i + 1, j + 1],
+  ];
+  let west = [
+    [i - 1, j - 1],
+    [i, j - 1],
+    [i + 1, j - 1],
+  ];
+  let east = [
+    [i - 1, j + 1],
+    [i, j + 1],
+    [i + 1, j + 1],
+  ];
+
+  let directions = [
+    [checkUnoccupied(north), [i - 1, j]],
+    [checkUnoccupied(south), [i + 1, j]],
+    [checkUnoccupied(west), [i, j - 1]],
+    [checkUnoccupied(east), [i, j + 1]],
+  ];
+
+  for (let i = 0; i < rounds % 4; i++) {
+    // console.log('shifting rounds')
+    directions.push(directions.shift());
+  }
+
+  // console.log(i,j);
+
+  for (const item of directions) {
+    // console.log(item);
+    if (item[0] === true) {
+      return item[1];
+    }
+  }
+
+  // console.log('no move detected?')
+  return "standing";
+}
+
+function checkUnoccupied(arr) {
+  for (const coord of arr) {
+    let [col, row] = coord;
+    if (elves[`${col},${row}`] !== undefined) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+prettyPrint();
+
+let unmoved = false;
+let i = 0;
+while(unmoved === false){
+  i+= 1;
+  simulateRound();
+}
+
+console.log(i);
+// prettyPrint(true);
+
+function simulateRound() {
+  unmoved = true;
+  let elfList = Object.keys(elves);
+  elfList = elfList
+    .map((el) => el.split(",").map((el) => +el))
+    .sort((a, b) => a[0] - b[0]);
+
+  let proposedMoves = {};
+
+  // first check if elf will propose a move
+  for (const elf of elfList) {
+    if (anyNeighbors(elf[0], elf[1])) {
+      let proposedMove = proposeMove(elf[0], elf[1]);
+      if (proposedMove !== "standing") {
+        if (proposedMoves[proposedMove] !== undefined) {
+          proposedMoves[proposedMove] = "collision";
+        } else {
+          proposedMoves[proposedMove] = `${elf[0]},${elf[1]}`;
+        }
+      }
+    }
+  }
+
+  // console.log(elves);
+
+  // console.log(proposedMoves);
+  // move elves that won't collide
+  for (const [key, value] of Object.entries(proposedMoves)) {
+    if (value !== "collision") {
+      unmoved = false;
+      elves[key] = "standing";
+      delete elves[value];
+    }
+  }
+
+  // console.log(elves);
+
+  // prettyPrint();
+
+  rounds += 1;
+}
+
+function prettyPrint(tally) {
+  let count = 0;
+
+  console.log("\n");
+  // find bounds
+  let elfList = Object.keys(elves);
+  elfList = elfList.map((el) => el.split(",").map((el) => +el));
+
+  // console.log(elfList)
+
+  let rowMin = Math.min(...elfList.map((el) => el[0]));
+  let rowMax = Math.max(...elfList.map((el) => el[0]));
+  let colMin = Math.min(...elfList.map((el) => el[1]));
+  let colMax = Math.max(...elfList.map((el) => el[1]));
+  // console.log(rowMin, rowMax,colMin,colMax)
+
+  for (let i = rowMin; i <= rowMax; i++) {
+    let str = "";
+
+    for (let j = colMin; j <= colMax; j++) {
+      if (elves[`${i},${j}`] !== undefined) {
+        str += "#";
+        // str += i
+      } else {
+        if (tally === true) {
+          count += 1;
+        }
+        str += ".";
+      }
+    }
+    console.log(str);
+  }
+  if (tally === true) {
+    console.log(count);
+  }
+}
